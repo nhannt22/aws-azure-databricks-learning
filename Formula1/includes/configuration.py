1a0f26c164f21b8c38e12004c8eaf2f5be7deea7
nhannt22 <nhannt22>
2024-04-20 05:20:51 +0000

    Add set-up and includes

---
+++ Formula1/includes/configuration.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formula1dlnt/raw"
+processed_folder_path = "/mnt/formula1dlnt/process"
+presentation_folder_path = "/mnt/formula1dlnt/presentation"
+
+# COMMAND ----------
+
+
